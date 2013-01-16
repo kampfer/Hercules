40b28878da8d56edf83886292085dd4ea9f61645
xiaojue <designsor@gmail.com>
2013-01-16 10:12:31 +0800

    add 12345

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 define('app',function(require,exports,module){
-    console.log('1234');
-	//11111111111111111111111111111111111
+    console.log('12345');
     var start = require('jobs/start');
     require('jobs/row');
 });
