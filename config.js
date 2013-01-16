b06bbf58a0073739f6674152e5d3fc1a245ca9ae
kampfer <l.w.kampfer@gmail.com>
2013-01-16 17:15:02 +0800

    1 add jquery ui 2 完成横向布局功能

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 define('config',function(require,exports,module){
     module.exports = {
-        timestamp:new Date().valueOf(),
+        //timestamp:new Date().valueOf(),
         alias:{
             '$':'modules/jquery-1.8.2.min',
             'drag':'modules/jquery.event.drag-2.2',
